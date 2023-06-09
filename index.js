6573ce3fda18e3fcbcf9d5dd280bfa026beacecf
valeriyavalera <leramutyk1@gmail.com>
2023-06-09 03:37:36 +0300

    .

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const Mongo = require("./setup/mongoose");
+const fs = require("fs");
 
-require('dotenv').config();
+const text = fs.readFileSync("./scenario.txt").toString();
 
-const { usersApiRouter } = require("./api/usersAPI");
-const { linksApiRouter } = require("./api/linksAPI");
+// res містить інформацію про початкові і кінцеві індекси фрагментів тексту кожного персонажа
+let a = text;
+const res = {};
+const b = [...a.matchAll(/^(Triss|Geralt|Max|Yennefer):/gim)];
+for (let i = 0; i < b.length; i += 1) {
+  const match = b[i];
+  const [_1, characterName] = match;
+  const { index } = match;
+  if (res[characterName]) {
+    res[characterName].push({
+      start: index,
+      end: b[i + 1] ? b[i + 1].index : a.length,
+    });
+    console.log;
+  } else {
+    res[characterName] = [
+      {
+        start: index,
+        end: b[i + 1] ? b[i + 1].index : -1,
+      },
+    ];
+  }
+}
 
+for (const characterName in res) {
+  const arrInd = res[characterName];
+  const result = [];
 
-const app = express();
-app.use(bodyParser.json());
+  //  в кожній ітерації циклу отримується фрагмент тексту для поточного персонажа, видаляється рядок з іменем персонажа, і цей фрагмент додається до масиву result
+  for (const { start, end } of arrInd) {
+    const line = text.substring(start, end);
+    // replace видаляє рядок з ім'ям персонажа
+    result.push(line.replace(`${characterName}: `, ""));
+  }
 
+  const linesText = result.join("\n");
+  fs.writeFileSync(`./characters/${characterName}.txt`, linesText);
 
-app.use(bodyParser.json());
-
-const setup = async () => {
-  await Mongo.setupDb(process.env.MONGO_DB_URI);
-
-  app.use(usersApiRouter);
-  app.use(linksApiRouter);
-
-  app.listen(process.env.PORT, () => {
-    console.log("server started ${process.env.PORT}");
-  });
-};
-  
-setup();
+  console.log(`${characterName}: `, result);
+  console.log(`${characterName}: `, result.length);
+}
